6c04c0cde84f6046d489c00b86fa51097e459db0
John Blank <veloblank@gmail.com>
2020-01-15 04:33:42 -0500

    Add scraper model and build scraper.

---
+++ app/models/scraper.rb
@@ -0,0 +1,40 @@
+class Scraper < ApplicationRecord
+  require 'open-uri'
+
+  GOODREADS = "https://www.goodreads.com/search?utf8=%E2%9C%93&query="
+  GOODREADS_BASE = "https://goodreads.com"
+  
+  attr_accessor :search_term
+  
+  def initialize(term)
+    @search_term = sanitize_search_term(term)
+  end
+  
+  def sanitize_search_term(term)
+    term.split(' ').join('+')
+  end
+  
+  def scrape_goodreads_for_book
+    doc = Nokogiri::HTML(open(GOODREADS + "#{self.search_term}"))
+    goodreads_partial = doc.css(".leftContainer table tr")[0].css("td a").attribute("href").value
+    goodreads_book_link = GOODREADS_BASE + goodreads_partial
+    doc2 = Nokogiri::HTML(open(goodreads_book_link))
+    
+    book_title = doc.css(".leftContainer table tr")[0].css("td a").attribute("title").value
+    book_author =doc2.css("a.authorName span").text
+    compressed_img_src = doc.css(".leftContainer table tr")[0].css("td a img").attribute("src").value
+    large_img_src = doc2.css(".bookCoverPrimary a img").attribute('src').value
+    num_pages = doc2.css("div#details .row").css("span")[1].text.split(' ')[0].to_i
+    num_words_low = num_pages * 250
+    num_words_high = num_pages * 295
+    book_hash = {
+      title: book_title,
+      author: book_author,
+      compressed_img: compressed_img_src,
+      large_img: large_img_src,
+      pages: num_pages,
+      words_low: num_words_low,
+      words_high: num_words_high
+    }
+  end
+end
---
+++ db/migrate/20200115011100_create_scrapers.rb
@@ -0,0 +1,9 @@
+class CreateScrapers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :scrapers do |t|
+      t.string :searchTerm
+
+      t.timestamps
+    end
+  end
+end
