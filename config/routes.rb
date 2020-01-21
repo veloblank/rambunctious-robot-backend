9f81ab8b1624f3303c72a03dce1c6c700cfeacbe
John Blank <veloblank@gmail.com>
2020-01-20 21:26:34 -0500

    Nest books resource routes in namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :reading_plans
-  resources :users
-  resources :books
   namespace :api do
     namespace :v1 do
       resources :searches
+      resources :books
     end
   end
 end
