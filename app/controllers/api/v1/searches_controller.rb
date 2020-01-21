f5dd69eb3357f64e8acbf5bdcc5f8e0108fadbd2
John Blank <veloblank@gmail.com>
2020-01-21 00:33:06 -0500

    Create AR instead of 'new'

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::SearchesController < ApplicationController
     scraper = GoodreadsScraper.new(search_term: params[:text])
     scraper.sanitize_search_term
     book_html = scraper.scrape_goodreads_for_book
-    book = Book.new(book_html)
+    book = Book.create(book_html)
     render json: book, status: 200
   end
 
